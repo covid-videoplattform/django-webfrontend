846f04dd0cd6fcc65c4bc0055cf2d576240a7b7e
Florian Zeidler <x-gean-site-git@fz.wtf>
2020-03-24 21:30:06 +0100

    Display fields in admin backend

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from .models import Appointment, StaffMember
 
 
-admin.site.register(Appointment)
-admin.site.register(StaffMember)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('last_modified', 'name', 'start_time',
+                    'end_time', 'staffmember', 'room_name')
+    list_filter = ['start_time']
+
+
+class StaffMemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(StaffMember, StaffMemberAdmin)
